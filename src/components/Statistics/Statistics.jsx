ebc7432f48eeed2d10f6850579aabd13ce81f335
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2023-02-26 20:43:17 +0200

    added code/styles for statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div className={styles.statistics}>
+    <ul className={styles.statisticsList}>
+      <li>
+        Good: <span>{good}</span>
+      </li>
+      <li>
+        Neutral: <span>{neutral}</span>
+      </li>
+      <li>
+        Bad: <span>{bad}</span>
+      </li>
+      <li>
+        Total: <span>{total}</span>
+      </li>
+      <li>
+        Positive feedback: <span>{positivePercentage}%</span>
+      </li>
+    </ul>
+  </div>
+);
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
+
+export default Statistics;
